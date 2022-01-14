2aed70973b76cae314abc6909c16e4c92ebe5eaa
NickNocc <haretakis4@gmail.com>
2022-01-14 13:31:48 -0800

    tag delete path paved

--- routes/api/tag-routes.js
+++ routes/api/tag-routes.js
@@ -69,7 +69,7 @@ router.put('/:id', (req, res) => {
   // update a tag's name by its `id` value
   Tag.update(
     {
-      tag_name: tag_name
+      tag_name: req.body.tag_name
     },
     {
       where: {
@@ -92,6 +92,22 @@ router.put('/:id', (req, res) => {
 
 router.delete('/:id', (req, res) => {
   // delete on tag by its `id` value
+  Tag.destroy({
+    where: {
+      id: req.body.id
+    }
+  })
+  .then(dbTagData => {
+    if (!dbTagData) {
+      res.status(404).json({ message: 'No tag found with this id' });
+      return;
+    }
+    res.json(dbTagData);
+  })
+  .catch(err => {
+    console.log(err);
+    res.status(500).json(err);
+  });
 });
 
 module.exports = router;
