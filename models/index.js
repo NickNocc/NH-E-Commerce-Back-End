e14f6b720e1d323ddd4fea523ff34ef4e57a7dd9
NickNocc <haretakis4@gmail.com>
2022-01-14 16:02:53 -0800

    Remove path doesnt work, but it seems to be bugged

--- models/index.js
+++ models/index.js
@@ -7,28 +7,24 @@ const ProductTag = require('./ProductTag');
 // Products belongsTo Category
 Product.belongsTo(Category, {
   foreignKey: 'category_id',
-  onDelete: 'SET NULL',
-  onUpdate: 'CASCADE'
+  hooks: true
 });
 // Categories have many Products
 Category.hasMany(Product, {
   foreignKey: 'category_id',
-  onDelete: 'SET NULL',
-  onUpdate: 'CASCADE'
+  hooks: true
 });
 // Products belongToMany Tags (through ProductTag)
 Product.belongsToMany(Tag, {
   through: ProductTag,
-  foreignKey: 'product_id',
-  onDelete: 'SET NULL',
-  onUpdate: 'CASCADE'
+  as: 'product_tag_id',
+  foreignKey: 'product_id'
 });
 // Tags belongToMany Products (through ProductTag)
 Tag.belongsToMany(Product, {
   through: ProductTag,
-  foreignKey: 'tag_id',
-  onDelete: 'SET NULL',
-  onUpdate: 'CASCADE'
+  as: 'product_tag_id',
+  foreignKey: 'tag_id'
 });
 
 module.exports = {
